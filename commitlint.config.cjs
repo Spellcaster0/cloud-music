595247ba364bf6ef4c4c2171ec06dfebc5e2dfc1
Suika <1556837013@qq.com>
2022-08-08 15:04:08 +0800

    chore: 项目基础
    eslint代码格式化, commit规范

---
+++ commitlint.config.cjs
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  parserPreset: 'conventional-changelog-conventionalcommits',
+  rules: {
+    'type-enum': [2, 'always', ['build', 'chore', 'docs', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test']],
+  },
+}
