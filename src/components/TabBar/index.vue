8e2368751752fa4be9477491168ced86ff0d5dee
Suika <1556837013@qq.com>
2022-08-17 19:09:53 +0800

    feat: 封装菜单组件

---
+++ src/components/TabBar/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <el-menu
+    :default-active="defaultActive"
+    class="tab-bar"
+    mode="horizontal"
+    :router="router"
+    @select="handleSelect"
+  >
+    <el-menu-item
+      v-for="item in menuList"
+      :key="item.index"
+      :index="item.index"
+    >
+      <span>{{ item.name }}</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup lang="ts">
+import type { Component } from 'vue'
+
+interface SelectFunc {
+  (key: string): void
+}
+
+interface PropsType {
+  menuList: Array<{ name: string, index: string, component?: Component }>,
+  defaultActive?: any,
+  router?: boolean,
+  handleSelect?: SelectFunc
+}
+
+const props = withDefaults(defineProps<PropsType>(), {
+  menuList: () => [
+    {
+      name: '个性推荐',
+      index: 'recommend'
+    },
+    {
+      name: '歌单',
+      index: 'playlist'
+    }
+  ],
+  defaultActive: '',
+  router: false
+})
+
+</script>
+
+<style lang="scss" scoped>
+.tab-bar {
+  border: none;
+  .el-menu-item {
+    height: 36px;
+    margin: 0 10px;
+    padding: 0 2px;
+    line-height: 36px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    &:hover,
+    &:focus {
+      color: #000;
+      background-color: #fff;
+    }
+  }
+  .is-active {
+    color: #000 !important;
+    font-size: 18px;
+    font-weight: 700;
+    background-color: #fff;
+    border-bottom: 2px solid $themeColor !important;
+  }
+}
+</style>
